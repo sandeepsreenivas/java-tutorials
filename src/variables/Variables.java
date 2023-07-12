f030dcdd4c510876849d652f842492ac5b690267
sandeepsreenivas <sreenivas.sandeep99@gmail.com>
2023-07-12 08:49:33 +0530

    first commit

---
+++ src/variables/Variables.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int a = 129;
+        boolean isRaining = true;
+        char b = 'H';
+        byte c = 120;
+        short s = 10000;
+        long l = 1000000000;
+        float price = 20.24f;
+        double sales = 10.0009;
+
+
+        System.out.println(a);
+        System.out.println(isRaining);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("Short is : " + s);
+        System.out.println("Long is : " + l);
+        System.out.println("Float is : " + price);
+        System.out.println("Double is : " + sales);
+    }
+
+}
