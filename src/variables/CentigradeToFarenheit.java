eb5902584175a033f11f51c391ae27908fbacafb
sandeepsreenivas <sreenivas.sandeep99@gmail.com>
2023-07-14 07:38:39 +0530

    Centigrade to faren

---
+++ src/variables/CentigradeToFarenheit.java
@@ -0,0 +1,16 @@
+package variables;
+
+/**
+ * 35 * 9/5 + 32
+ */
+
+public class CentigradeToFarenheit {
+    public static void main(String[] args) {
+
+        float centigrade = 35;
+        float farenheit = centigrade * (float) 9/5;
+        farenheit = farenheit + 32;
+        System.out.println(farenheit);
+
+    }
+}
